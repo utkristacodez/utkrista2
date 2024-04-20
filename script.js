1b05db07829f4277c6d074f54ccc2c0e15f84d93
utkristacodez <150587658+utkristacodez@users.noreply.github.com>
2024-04-20 13:37:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
 
 
-function showPopup() {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'block';
-    setTimeout(() => {
-        popup.classList.add('active');
-    }, 10);
-}
-
-function closePopup() {
-    const popup = document.getElementById('popup');
-    popup.classList.remove('active');
-    setTimeout(() => {
-        popup.style.display = 'none';
-    }, 300);
-}
-
